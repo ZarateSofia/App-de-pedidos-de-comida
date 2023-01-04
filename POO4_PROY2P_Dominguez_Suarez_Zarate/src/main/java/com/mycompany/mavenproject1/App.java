38d93f6fc88135cca18b19def611e654694d5e84
ZarateSofia <DELL@DESKTOP-N9P7D59>
2023-01-04 02:29:53 -0500

    Ventana 1

--- POO4_PROY2P_Dominguez_Suarez_Zarate/src/main/java/com/mycompany/mavenproject1/App.java
+++ POO4_PROY2P_Dominguez_Suarez_Zarate/src/main/java/com/mycompany/mavenproject1/App.java
@@ -6,31 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
-
     private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmLoader=new FXMLLoader(App.class.getResource("InicioSesion.fxml"));
+        Parent root=fxmLoader.load();
+        scene=new Scene(root,640,600);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Inicio de Sesion");
+ 
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
         launch();
     }
