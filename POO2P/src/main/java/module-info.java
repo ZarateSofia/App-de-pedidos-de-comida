077b07d57dcdf329f334e96c89ffbb6bfeeab094
Sofia Zarate <szarate@espol.edu.ec>
2023-01-21 21:16:14 -0500

    cambiar nombre

--- POO2P/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.pooespol.poo2p {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.pooespol.poo2p to javafx.fxml;
-    exports com.pooespol.poo2p;
-}
